4b4fd51d2d28ea3e1df2a9dd53f4f409c31de498
Martin Černoch <cernochm@outlook.cz>
2023-05-19 23:25:31 +0200

    feat(nuxt.config): add google-fonts module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,11 +4,11 @@ export default defineNuxtConfig({
   //   SUPABASE_KEY: process.env.SUPABASE_KEY,
   //   SUPABASE_URL: process.env.SUPABASE_URL,
   // },
-  modules: ['@nuxtjs/supabase', '@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
+  modules: ['@nuxtjs/supabase', '@nuxtjs/tailwindcss', '@nuxtjs/google-fonts'],
   app: {
     head: {
       meta: [
-        { name: 'description', content: 'Phisher admin portal.' }
+        { name: 'description', content: 'Phisher administrator portal.' }
       ],
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
