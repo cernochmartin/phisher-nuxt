796f8aa0d40453b6d2e19eca4227536d5b186703
Martin Černoch <cernochm@outlook.cz>
2023-05-19 23:33:22 +0200

    feat: playing with google fonts and meta content

--- app.vue
+++ app.vue
@@ -6,8 +6,6 @@
   </div>
 </template>
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
-
 * {
   font-family: 'Montserrat';
 }
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,10 +5,16 @@ export default defineNuxtConfig({
   //   SUPABASE_URL: process.env.SUPABASE_URL,
   // },
   modules: ['@nuxtjs/supabase', '@nuxtjs/tailwindcss', '@nuxtjs/google-fonts'],
+  googleFonts: {
+    families: {
+      Montserrat: [400]
+    },
+    display: 'swap'
+  },
   app: {
     head: {
       meta: [
-        { name: 'description', content: 'Phisher administrator portal.' }
+        { name: 'description', content: "Phisher administrator portal let's you manageme your Phisher software, see the latest features, add, delete or make changes for users, change a subscription plan or get help by our assistant." }
       ],
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
