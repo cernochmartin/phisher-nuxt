f883e01a4b0505dcf76dc1838cd32be5933481cd
Martin Černoch <cernochm@outlook.cz>
2023-05-27 16:17:44 +0200

    wip(settings)

--- pages/settings.vue
+++ pages/settings.vue
@@ -1,17 +1,43 @@
 <script setup>
 useHead({
-  title: 'Phisher | Admin Portal: Settings'
+  title: 'Phisher | Admin Portal: Admin settings'
 })
 </script>
 <template>
-  <h2 class="text-4xl pb-20">Settings</h2>
-  <h3 class="text-2xl pb-4">Change your settings</h3>
-  <article class="settings w-full"></article>
+  <h2 class="text-4xl pb-20">Admin settings</h2>
+  <h3 class="text-2xl pb-5">Change your settings</h3>
+  <article class="settings">
+    <div class="flex gap-6 justify-center">
+      <div class="flex flex-col gap-6">
+        <span>First name:</span>
+        <span>Surname:</span>
+      </div>
+      <div class="flex flex-col gap-6">
+        <input type="text" placeholder="First name" />
+        <input type="text" placeholder="Surname" />
+      </div>
+    </div>
+    <div class="flex w-full justify-center pt-7">
+      <button type="submit" class="bg-blue-500 text-white w-20 p-1.5 rounded-sm">Submit</button>
+    </div>
+  </article>
 </template>
 <style scoped>
 .settings {
   background-color: #f2f2f2;
-  height: 560px;
+  width: 620px;
+  height: 640px;
+  padding: 24px;
   border-radius: 8px;
+}
+
+span {
+  padding: 5px;
+}
+
+input {
+  border: 1px solid black;
+  border-radius: 4px;
+  padding: 4px;
 }
 </style>
